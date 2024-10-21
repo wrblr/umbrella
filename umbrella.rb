d052f75868801c6aae9905f29f10b561e1423b80
wrblr <wrbell@uchicago.edu>
2024-10-21 22:43:29 +0000

    pirate weather api working

--- umbrella.rb
+++ umbrella.rb
@@ -2,10 +2,11 @@ require "http"
 require "json"
 require "dotenv/load"
 
-# Hidden variables
+# Hidden variables:
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 gmaps_key = ENV.fetch("GMAPS_KEY")
 
+# User interaction:
 pp "Where are you located?"
 
 user_location = gets.chomp.gsub(" ", "%20")
@@ -13,6 +14,7 @@ user_location = gets.chomp.gsub(" ", "%20")
 #user_location = "Chicago"
 pp user_location
 
+# Use user response:
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
 resp = HTTP.get(maps_url)
@@ -24,20 +26,23 @@ results = parsed_resp.fetch("results")
 first_result = results.at(0)
 geo = first_result.fetch("geometry")
 loc = geo.fetch("location")
-pp latitude = loc.fetch("lat")
-pp longitude = loc.fetch("lng")
+pp latitude = loc.fetch("lat").to_s
+pp longitude = loc.fetch("lng").to_s
 
 
-# # Assemble the full URL string by adding the first part, the API token, and the last part together
-# pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/" + latitude + "," + longitude
 
-# # Place a GET request to the URL
-# raw_response = HTTP.get(pirate_weather_url)
+# Place a GET request to the URL
+raw_response = HTTP.get(pirate_weather_url)
 
-# parsed_response = JSON.parse(raw_response)
+# Parse response to JSON format
+parsed_response = JSON.parse(raw_response)
 
-# currently_hash = parsed_response.fetch("currently")
+# Extract current weather hash
+currently_hash = parsed_response.fetch("currently")
 
-# current_temp = currently_hash.fetch("temperature")
+# Extract current temperature
+current_temp = currently_hash.fetch("temperature")
 
-# puts "The current temperature is " + current_temp.to_s + "."
+puts "The current temperature is " + current_temp.to_s + "."
